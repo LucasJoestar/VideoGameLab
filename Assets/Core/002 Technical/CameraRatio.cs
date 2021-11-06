09501fb5385e73b6b0d6a69d640eb864e8b09550
LucasJoestar <lucasjoestar@outlook.fr>
2021-11-06 11:40:12 +0100

    Camera Aspect Ratio

--- Assets/Core/002 Technical/CameraRatio.cs
+++ Assets/Core/002 Technical/CameraRatio.cs
@@ -1,18 +1,60 @@
-using System.Collections;
-using System.Collections.Generic;
+// ===== Video Game Lab Game Jam - https://github.com/LucasJoestar/VideoGameLab ===== //
+//
+// Notes:
+//
+// ================================================================================== //
+
 using UnityEngine;
 
-public class CameraRatio : MonoBehaviour
+namespace Shmup
 {
-    // Start is called before the first frame update
-    void Start()
+    public class CameraRatio : MonoBehaviour
     {
-        
-    }
+        #region Global Members
+        [SerializeField, Range(.1f, 1f)] private float gameAspect = .8f / 3f;
+        [SerializeField] private new Camera camera = null;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private float lastAspect = 1f;
+
+        // -----------------------
+
+        private void Update()
+        {
+            float _aspect = (float)Screen.width / Screen.height;
+            if (_aspect == lastAspect)
+                return;
+
+            lastAspect = _aspect;
+            float _scaledHeight = _aspect / gameAspect;
+
+            // Set camera aspect.
+            if (_scaledHeight < 1f)
+            {
+                Rect _rect = new Rect()
+                {
+                    x = 0f,
+                    y = (1f - _scaledHeight) / 2f,
+                    width = 1f,
+                    height = _scaledHeight
+                };
+
+                camera.rect = _rect;
+            }
+            else
+            {
+                float scaleWidth = 1f / _scaledHeight;
+
+                Rect _rect = new Rect()
+                {
+                    x = (1f - scaleWidth) / 2f,
+                    y = 0,
+                    width = scaleWidth,
+                    height = 1f
+                };
+
+                camera.rect = _rect;
+            }
+        }
+        #endregion
     }
 }
---
+++ Assets/Core/002 Technical/CodeSamples.cs
@@ -0,0 +1,61 @@
+// ===== Enhanced Editor - https://github.com/LucasJoestar/EnhancedEditor ===== //
+// 
+// Notes:
+//
+// ============================================================================ //
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+internal class CodeSamples : EditorWindow
+{
+    // --- Inter-Region Title --- \\
+
+    // ===== Inside Region Title ===== \\
+
+    // -------------------------------------------
+    // Header
+    // -------------------------------------------
+
+    // ----- Local Methods ----- \\
+
+    /// <inheritdoc cref="Doc(bool)"/>
+    public void Doc()
+    {
+        using (var _scope = new GUILayout.HorizontalScope())
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// This is a documentation method.
+    /// </summary>
+    /// <param name="_value">Random value.</param>
+    /// <returns><inheritdoc cref="Doc(bool)" path="/param[@name='_value']"/></returns>
+    public bool Doc(bool _value) => _value;
+
+    // -----------------------
+
+    /// <summary>
+    /// Returns the first <see cref="CodeSamples"/> currently on screen.
+    /// <br/>
+    /// Creates and shows a new instance if there is none.
+    /// </summary>
+    /// <returns><see cref="CodeSamples"/> instance on screen.</returns>
+    [MenuItem("Enhanced Editor/My Window", false, 101)]
+    public static CodeSamples GetWindow()
+    {
+        CodeSamples _window = GetWindow<CodeSamples>("My Window");
+        _window.Show();
+
+        return _window;
+    }
+
+    /// <summary>
+    /// Contains multiple <see cref="CodeSamples"/>-related extension methods.
+    /// </summary>
+    private class CodeSamplesExtensions { }
+}
+#endif
