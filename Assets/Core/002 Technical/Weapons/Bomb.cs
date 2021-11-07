853a78a0db04b53f79eeb192ca9151f6717eb041
Alexis Thiébaut <thiebautalexis01@gmail.com>
2021-11-07 22:10:02 +0100

    BombFlash

--- Assets/Core/002 Technical/Weapons/Bomb.cs
+++ Assets/Core/002 Technical/Weapons/Bomb.cs
@@ -5,6 +5,8 @@
 // ================================================================================== //
 
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 namespace Shmup
 {
@@ -12,11 +14,23 @@ namespace Shmup
     {
         #region Fields and properties
         [SerializeField, Range(.01f, 1.0f)] private float waitingDelay = 0.05f;
+        [SerializeField] private Image flashImage = null;
+        [SerializeField] private AnimationCurve flashCurve = new AnimationCurve();
+        [SerializeField] private float flashDuration = 1.0f;
+        private Sequence flashSequence = null;
         #endregion
 
         #region Methods
         public override void Fire()
         {
+            if(flashSequence.IsActive())
+            {
+                flashSequence.Kill();
+            }
+            flashSequence = DOTween.Sequence();
+            flashSequence.Join(flashImage.DOFade(1.0f, flashDuration).SetEase(flashCurve));
+            flashSequence.Play();
+
             for (int i = 0; i < EnemyController.enemies.Count; i++)
             {
                 EnemyController.enemies[i].ApplyBombBehaviour(waitingDelay * i, weaponsData.Damages);
